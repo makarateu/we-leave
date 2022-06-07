2dcfa41695b96e4f7f55c589c6429d9010cc371f
veng <veng@vengs-MacBook-Pro.local>
2022-06-07 11:12:31 +0700

    adds menu and employee list features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import ReactDOM from "react-dom";
 import './App.css';
+import Logo from './logo.svg'
+import LeaveCalendar from './components/leave-calendar/LeaveCalendar';
+import Menu from './components/menu/Menu';
+import EmployeeList from './components/employee-list/EmployeeList';
+import Button from '@mui/material/Button';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Layout from "./components/layout/layout";
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      
+      <HeaderBar>
+        <Menu/>
+        <a href="/" className="logo">
+          <img src={Logo} alt="logo"></img>
         </a>
-      </header>
+
+        <HeaderItem>
+          <DropDownMenu/>
+        </HeaderItem>
+      </HeaderBar>
+
+      <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<LeaveCalendar/>}></Route>
+          <Route path="LeaveCalendar" element={<LeaveCalendar/>} />
+          <Route path="EmployeeList" element={<EmployeeList/>} />
+          </Route>
+      </Routes>
+    </BrowserRouter>
+
     </div>
+
   );
 }
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+function DropDownMenu() {
+
+  function DropDownItem(props){
+    return (
+      <a href='#' className='remarks-item'>
+        <span className='icon-button'> {props.leftIcon} </span>
+          {props.children}
+        <span className='icon-right'> {props.rightIcon} </span>
+      </a>
+    )
+  }
+
+  return (
+    <div className='dropdown'>
+      <DropDownItem><p className='remark-text' id='current-day'> CURRENT DAY </p></DropDownItem>
+      <DropDownItem> <p className='remark-text' id='leave-taken'> LEAVE :: TAKEN </p></DropDownItem>
+      <DropDownItem> <p className='remark-text' id='leave-pending'> LEAVE :: PENDING APPROVAL </p> </DropDownItem>
+      <DropDownItem><p className='remark-text' id='leave-approved'> LEAVE :: APPROVED </p> </DropDownItem>
+      <DropDownItem> <p className='remark-text' id='holiday'> PUBLIC HOLIDAY </p> </DropDownItem>
+    </div>
+  )
+}
+
+function HeaderBar(props) {
+  return (
+    <nav className="headerbar">
+      <ul className='headerbar-nav'> {props.children}</ul>
+    </nav>
+  )
+}
+
+function HeaderItem(props) {
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    <li className='header-item'>
+      <a href='#' className='item' onClick={() => setOpen(!open)}>
+        <Button className='remark-button'> Remarks <ArrowDropDownIcon></ArrowDropDownIcon> </Button>
+      </a>
+
+      {open && props.children}
+    </li>
+  )
+}
+
 
 export default App;
